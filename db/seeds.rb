cc238d2daddd2a77645e22ffb3a1c4d24924d270
Vladimir Polukhin <vovanmozg@gmail.com>
2022-05-03 22:53:21 +0300

    Generate more data

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,3 +19,22 @@ User.create!(
   preferred_price_range: 35_000...40_000,
   preferred_brands: [BRANDS['Alfa Romeo'], BRANDS['Volkswagen']],
 )
+#
+# # Brands
+# 1000.times do
+#   Brand.create!(
+#     name: (0...10).map { ([' ']+('A'..'Z').to_a)[rand(26)] }.join
+#   )
+# end
+#
+# last_brand_id = Brand.maximum(:id)
+#
+# # Cars
+# 1000000.times do |i|
+#   print '.' if i % 100 == 0
+#   Car.create!(
+#     model: (0...10).map { ([' ']+('a'..'z').to_a)[rand(26)] }.join,
+#     brand_id: rand(1..last_brand_id),
+#     price: rand(1000..100000)
+#   )
+# end
